b2b7eaa41b4ac2d49c6818864dd7b4b9a9bfee1f
Andre <ank2.qc@gmail.com>
2023-06-04 20:38:28 -0300

    user_id

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    puts post_params
+    @post = Post.new(post_params)
+    
+    if @post.save
+      redirect_to new_post_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end  
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, user_id: current_user.id)
+  end
+  
+end
